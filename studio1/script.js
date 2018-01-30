2b90861afadc020ade4c1b56f41794832a628c8a
smobrique <35280392+smobrique@users.noreply.github.com>
2018-01-29 16:49:32 -0800

    Update script.js

--- studio1/script.js
+++ studio1/script.js
@@ -18,12 +18,11 @@ document.f.onreset = clearForm;
         else {
             var myMsg = document.getElementById('myMsg');
             myMsg.innerHTML = 'Alright <em>' + userName + '</em>, here is your guide on how to survive Davis campus life!' +
-
-                '<li>Before the quarter starts, you have to rub an egghead at least <em>' + userNum + '</em> times. Anything less means that you will fail your exams.</li>' +
-                '<li>Trying to avoid the wrath of your professors? Bring them some <em>' + userCandy + '</em> and start your class off right. Your classmates will thank you later.</li>' +
-                '<li>Try not to get too caught off guard by the winters in Davis. Rumor has it that winds can go up to <em>' + userWindSpeed + '</em> miles per hour with a 30 degree windchill. Bringing a light cardigan will do the trick.</li>' +
-                '<li>Lastly, be sure to plan your trips to campus accordingly. You never know if the bus driver is <em>' + userVerb +'ing</em>.</li>' +
-                '<br><em>DISCLAIMER:</em> This guide has not been properly tested and may only result in a 0.0000001% success rate. Proceed with caution.';
+            '<li>Before the quarter starts, you have to rub an egghead at least <em>' + userNum + '</em> times. Anything less means that you will fail your exams.</li>' +
+            '<li>Trying to avoid the wrath of your professors? Bring them some <em>' + userCandy + '</em> and start your class off right. Your classmates will thank you later.</li>' +
+            '<li>Try not to get too caught off guard by the winters in Davis. Rumor has it that winds can go up to <em>' + userWindSpeed + '</em> miles per hour with a 30 degree windchill. Bringing a light cardigan will do the trick.</li>' +
+            '<li>Lastly, be sure to plan your trips to campus accordingly. You never know if the bus driver is <em>' + userVerb +'ing</em>.</li>' +
+            '<br><em>DISCLAIMER:</em> This guide has not been properly tested and may only result in a 0.0000001% success rate. Proceed with caution.';
             myMsg.className = 'show';
           }
         return false;
