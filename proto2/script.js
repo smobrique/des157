136187d9f869d3b0268f4ae55eac6ff21e8ed95f
smobrique <35280392+smobrique@users.noreply.github.com>
2018-03-08 09:56:11 -0800

    Update script.js

--- proto2/script.js
+++ proto2/script.js
@@ -27,3 +27,42 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+/* narrative overlays */
+var narr01 = document.getElementById('narr01');
+var narr02 = document.getElementById('narr02');
+var narr03 = document.getElementById('narr03');
+
+var narrative1 = document.getElementById('narrative1');
+var narrative2 = document.getElementById('narrative2');
+var narrative3 = document.getElementById('narrative3');
+
+var close = document.getElementById('close');
+var close2 = document.getElementById('close2');
+var close3 = document.getElementById('close3');
+
+/* overlays*/
+narr01.addEventListener('click', function(){
+  narrative1.style.display="block";
+})
+
+close.addEventListener('click', function(){
+  narrative1.style.display="none";
+})
+
+narr02.addEventListener('click', function(){
+  narrative2.style.display="block";
+})
+
+close2.addEventListener('click', function(){
+  narrative2.style.display="none";
+})
+
+narr03.addEventListener('click', function(){
+  narrative3.style.display="block";
+})
+
+close3.addEventListener('click', function(){
+  narrative3.style.display="none";
+})
