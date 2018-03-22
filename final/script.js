2a6184e99918915b6ea1ff3a8d7072bc481e97ff
smobrique <35280392+smobrique@users.noreply.github.com>
2018-03-21 23:16:36 -0700

    Update script.js

--- final/script.js
+++ final/script.js
@@ -1,5 +1,3 @@
-console.log("reading js");
-
 'use strict'
 
 /* homepage slides*/
@@ -23,11 +21,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
 
 
